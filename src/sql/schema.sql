440497ab06a06cb8415ab39e136ad24e5baa9904
dantecatalfamo <dante.catalfamo@gmail.com>
2023-09-26 23:32:39 -0400

    Some linting

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,30 +1,30 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS interfaces (
-  id INTEGER PRIMARY KEY,
-  name TEXT NOT NULL UNIQUE,
-  comment TEXT,
-  privkey TEXT NOT NULL UNIQUE,
-  hostname TEXT,
-  port INTEGER,
-  address TEXT NOT NULL UNIQUE,
-  prefix INTEGER,
-  psk TEXT
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    comment TEXT,
+    privkey TEXT NOT NULL UNIQUE,
+    hostname TEXT,
+    port INTEGER,
+    address TEXT NOT NULL UNIQUE,
+    prefix INTEGER,
+    psk TEXT
 );
 
 CREATE TABLE IF NOT EXISTS peers (
-  id INTEGER PRIMARY KEY,
-  interface1 INTEGER NOT NULL,
-  interface2 INTEGER NOT NULL,
-  FOREIGN KEY (interface1) REFERENCES interfaces(id) ON DELETE CASCADE,
-  FOREIGN KEY (interface2) REFERENCES interfaces(id) ON DELETE CASCADE,
-  UNIQUE (interface1, interface2)
+    id INTEGER PRIMARY KEY,
+    interface1 INTEGER NOT NULL,
+    interface2 INTEGER NOT NULL,
+    FOREIGN KEY (interface1) REFERENCES interfaces (id) ON DELETE CASCADE,
+    FOREIGN KEY (interface2) REFERENCES interfaces (id) ON DELETE CASCADE,
+    UNIQUE (interface1, interface2)
 );
 
 CREATE TABLE IF NOT EXISTS allowed_ips (
-  id INTEGER PRIMARY KEY,
-  peer INTEGER,
-  address TEXT,
-  prefix INTEGER,
-  FOREIGN KEY (peer) REFERENCES peers(id) ON DELETE CASCADE
+    id INTEGER PRIMARY KEY,
+    peer INTEGER,
+    address TEXT,
+    prefix INTEGER,
+    FOREIGN KEY (peer) REFERENCES peers (id) ON DELETE CASCADE
 );
