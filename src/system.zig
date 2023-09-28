0f45d1bcc16fcad6728aa78d76d5cc62c5f1b52b
dantecatalfamo <dante.catalfamo@gmail.com>
2023-09-28 14:04:16 -0400

    Remove unused getInterface command

--- src/system.zig
+++ src/system.zig
@@ -55,26 +55,6 @@ pub const System = struct {
         return try self.db.exec_returning_int(query, .{ name });
     }
 
-    pub fn getInterface(self: System, id: u64) !Interface {
-        const query = "SELECT id, name, comment, privkey, hostname, port, address, prefix, psk FROM interfaces WHERE id = ?";
-        const stmt = try self.db.prepare_bind(query, .{ id });
-        if (!try stmt.step()) {
-            return error.NoInterface;
-        }
-        return  Interface{
-            .allocator = self.allocator,
-            .id = stmt.uint(0),
-            .name = if (stmt.text(1)) |str| try self.allocator.dupe(u8, str) else return error.InvalidInterfaceName,
-            .comment = if (stmt.text(2)) |str| try self.allocator.dupe(u8, str) else null,
-            .privkey = if (stmt.text(3)) |str| try self.allocator.dupe(u8, str) else return error.InvalidInterfacePrivkey,
-            .hostname = if (stmt.text(4)) |str| try self.allocator.dupe(u8, str) else null,
-            .port = if (stmt.int(5) != 0) @intCast(stmt.int(5)) else null,
-            .address = if (stmt.text(6)) |str| try self.allocator.dupe(u8, str) else return error.InvalidInterfaceAddress,
-            .prefix = if (stmt.int(7) != 0) @intCast(stmt.int(7)) else return error.InvalidInterfacePrefix,
-            .preshared_key = if (stmt.text(8)) |str| try self.allocator.dupe(u8, str) else null,
-        };
-    }
-
     pub fn addRouter(self: System, router_id: u64, client_id: u64) !void {
         const query = "SELECT address, prefix FROM interfaces WHERE id = ?";
         const stmt = try self.db.prepare(query, null);
@@ -334,33 +314,5 @@ pub const System = struct {
 
     pub fn verifyPrivkey(privkey: []const u8) !bool {
         return 32 == try std.base64.standard.Decoder.calcSizeForSlice(privkey);
-    }
-};
-
-pub const Interface = struct {
-    allocator: mem.Allocator,
-    id: ?u64,
-    name: []const u8,
-    comment: ?[]const u8,
-    privkey: []const u8,
-    hostname: ?[]const u8 = null,
-    port: ?u16 = null,
-    address: []const u8,
-    prefix: u6,
-    preshared_key: ?[]const u8 = null,
-
-    pub fn deinit(self: Interface) void {
-        self.allocator.free(self.name);
-        if (self.comment) |m| self.allocator.free(m);
-        self.allocator.free(self.privkey);
-        if (self.hostname) |m| self.allocator.free(m);
-        self.allocator.free(self.address);
-        if (self.preshared_key) |m| self.allocator.free(m);
-    }
-
-    pub fn kp(self: Interface) !keypair.KeyPair {
-        var priv: [44]u8 = undefined;
-        @memcpy(&priv, self.privkey);
-        return try keypair.fromBase64PrivateKey(priv);
     }
 };
